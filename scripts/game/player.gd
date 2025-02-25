54d9362f9febdc358fd7a4d3bff69b6ef5e780f4
Huy Truong <nguyenhuytruong9112k@gmail.com>
2025-02-26 04:19:45 +0700

    update

--- scripts/game/player.gd
+++ scripts/game/player.gd
@@ -183,8 +183,8 @@ func show_bonus(txt, type=0): # last trick
 		# napoli
 	var p = self.global_position
 	var screen_size_width = get_viewport().size.x / 2
-	print('p.xxxx', p.x, screen_size_width)
-	if p.x > screen_size_width / 2:
+
+	if p.x > screen_size_width:
 		# player in the left
 		self.bonus_info_pn.position.x = default_pos_bonus.x - 230
 		self.bonus_info_pn.pivot_offset = Vector2(bonus_info_pn.size.x, 0)
