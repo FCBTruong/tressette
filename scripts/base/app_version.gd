a6dd08abcf21ffcc92f6e94c338305bfd982ff7f
Huy Truong <nguyenhuytruong9112k@gmail.com>
2025-04-11 21:05:57 +0700

    update app version

--- scripts/base/app_version.gd
+++ scripts/base/app_version.gd
@@ -9,8 +9,8 @@ var reviewing_version = 0
 var cdn_version = 0
 
 # INCREASE WHEN BUILD, should edit when upload new build
-const ANDROID_CODE_VERSION: int = 8
-const IOS_CODE_VERSION: int = 5
+const ANDROID_CODE_VERSION: int = 9
+const IOS_CODE_VERSION: int = 6
 var my_code_version
 
 func test():
