018aea2d8b156c6962ae86050571db20d2a6162e
Huy Truong <nguyenhuytruong9112k@gmail.com>
2025-08-13 18:55:17 +0700

    add vip

--- scripts/game/chat/emo_chat_node.gd
+++ scripts/game/chat/emo_chat_node.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @onready var anim_player = find_child("AnimPlayer")
+@onready var vip_img = find_child("VipImg")
 var emo_id
 var mgr
 # Called when the node enters the scene tree for the first time.
@@ -14,13 +15,26 @@ func _process(delta: float) -> void:
 
 
 func _on_click():
+	if not is_active:
+		return
 	mgr.hide_emo()
 	var pkg = g.v.game_constants.PROTOBUF.PACKETS.InGameChatEmoticon.new()
 	pkg.set_emoticon(emo_id)
 	g.v.game_client.send_packet(g.v.game_constants.CMDs.CHAT_EMOTICON, pkg.to_bytes())
 	pass
 
+var is_active
 func set_emo(emo_id, mgr):
+	is_active = true
+	if emo_id in g.v.game_server_config.emo_vip_ids:
+		if not g.v.player_info_mgr.is_user_vip():
+			is_active = false
+	if not is_active:
+		vip_img.visible = true
+		self.modulate.a = 0.5
+	else:
+		vip_img.visible = false
+		self.modulate.a = 1
 	self.mgr = mgr
 	self.emo_id = emo_id
 	anim_player.play(str(emo_id))
--- scripts/game_server_config.gd
+++ scripts/game_server_config.gd
@@ -6,7 +6,7 @@ var time_thinking_in_turn: float = 5
 var exp_levels = []
 var fee_mode_no_bet: int
 var level_rewards = []
-
+var emo_vip_ids = [7,8,9,10,11,12,13,14,15]
 func convert_exp_to_level(exp) -> int:
 	var level = 1  # Default to level 1 if exp is below the first threshold
 	for i in range(1, len(exp_levels)):
